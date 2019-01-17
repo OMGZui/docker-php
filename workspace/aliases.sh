84f38f0174d8ae8eedc68bfd2407c538032e5ac8
小粽子 <862275679@qq.com>
2019-01-17 14:54:08 +0800

    新版本

--- workspace/aliases.sh
+++ workspace/aliases.sh
@@ -33,12 +33,118 @@ alias lsd="ls -lF ${colorflag} | grep --color=never '^d'"
 # Always use color output for `ls`
 alias ls="command ls ${colorflag}"
 
+# Commonly Used Aliases
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ~="cd ~" # `cd` is probably faster to type though
+alias -- -="cd -"
+alias home="cd ~"
+
+alias h="history"
+alias j="jobs"
+alias e='exit'
+alias c="clear"
+alias cla="clear && ls -l"
+alias cll="clear && ls -la"
+alias cls="clear && ls"
+alias code="cd /var/www"
+alias ea="vi ~/aliases.sh"
+
 # Always enable colored `grep` output
 # Note: `GREP_OPTIONS="--color=auto"` is deprecated, hence the alias usage.
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
 
+alias art="php artisan"
+alias artisan="php artisan"
+alias cdump="composer dump-autoload -o"
+alias composer:dump="composer dump-autoload -o"
+alias db:reset="php artisan migrate:reset && php artisan migrate --seed"
+alias dusk="php artisan dusk"
+alias fresh="php artisan migrate:fresh"
+alias migrate="php artisan migrate"
+alias refresh="php artisan migrate:refresh"
+alias rollback="php artisan migrate:rollback"
+alias seed="php artisan db:seed"
+alias serve="php artisan serve --quiet &"
+
+alias phpunit="./vendor/bin/phpunit"
+alias pu="phpunit"
+alias puf="phpunit --filter"
+alias pud='phpunit --debug'
+
+alias cc='codecept'
+alias ccb='codecept build'
+alias ccr='codecept run'
+alias ccu='codecept run unit'
+alias ccf='codecept run functional'
+
+alias g="gulp"
+alias npm-global="npm list -g --depth 0"
+alias ra="reload"
+alias reload="source ~/.aliases && echo \"$COL_GREEN ==> Aliases Reloaded... $COL_RESET \n \""
+alias run="npm run"
+alias tree="xtree"
+
 # Xvfb
 alias xvfb="Xvfb -ac :0 -screen 0 1024x768x16 &"
 
+# requires installation of 'https://www.npmjs.com/package/npms-cli'
+alias npms="npms search"
+# requires installation of 'https://www.npmjs.com/package/package-menu-cli'
+alias pm="package-menu"
+# requires installation of 'https://www.npmjs.com/package/pkg-version-cli'
+alias pv="package-version"
+# requires installation of 'https://github.com/sindresorhus/latest-version-cli'
+alias lv="latest-version"
+
+# git aliases
+alias gaa="git add ."
+alias gd="git --no-pager diff"
+alias git-revert="git reset --hard && git clean -df"
+alias gs="git status"
+alias whoops="git reset --hard && git clean -df"
+alias glog="git log --oneline --decorate --graph"
+alias gsh="git show"
+alias grb="git rebase -i"
+alias gbr="git branch"
+alias gc="git commit"
+alias gck="git checkout"
+
+# Create a new directory and enter it
+function mkd() {
+    mkdir -p "$@" && cd "$@"
+}
+
+function md() {
+    mkdir -p "$@" && cd "$@"
+}
+
+function xtree {
+    find ${1:-.} -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'
+}
+
+# `tre` is a shorthand for `tree` with hidden files and color enabled, ignoring
+# the `.git` directory, listing directories first. The output gets piped into
+# `less` with options to preserve color and line numbers, unless the output is
+# small enough for one screen.
+function tre() {
+	tree -aC -I '.git|node_modules|bower_components' --dirsfirst "$@" | less -FRNX;
+}
+
+# Determine size of a file or total size of a directory
+function fs() {
+	if du -b /dev/null > /dev/null 2>&1; then
+		local arg=-sbh;
+	else
+		local arg=-sh;
+	fi
+	if [[ -n "$@" ]]; then
+		du $arg -- "$@";
+	else
+		du $arg .[^.]* ./*;
+	fi;
+}
